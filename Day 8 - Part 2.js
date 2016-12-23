6526bb8e544c51de4699722d38ba6d65f6ace9ab
qtotuan <quynh.totuan@gmail.com>
2016-12-23 17:29:47 -0500

    add new solutions for advent of code

---
+++ Day 8 - Part 2.js
@@ -0,0 +1,259 @@
+\var input = `rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 6
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 5
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 4
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 7
+rect 3x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 3
+rect 1x2
+rotate row y=1 by 13
+rotate column x=0 by 1
+rect 2x1
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 3x1
+rotate row y=0 by 18
+rotate column x=13 by 1
+rotate column x=7 by 2
+rotate column x=2 by 3
+rotate column x=0 by 1
+rect 17x1
+rotate row y=3 by 13
+rotate row y=1 by 37
+rotate row y=0 by 11
+rotate column x=7 by 1
+rotate column x=6 by 1
+rotate column x=4 by 1
+rotate column x=0 by 1
+rect 10x1
+rotate row y=2 by 37
+rotate column x=19 by 2
+rotate column x=9 by 2
+rotate row y=3 by 5
+rotate row y=2 by 1
+rotate row y=1 by 4
+rotate row y=0 by 4
+rect 1x4
+rotate column x=25 by 3
+rotate row y=3 by 5
+rotate row y=2 by 2
+rotate row y=1 by 1
+rotate row y=0 by 1
+rect 1x5
+rotate row y=2 by 10
+rotate column x=39 by 1
+rotate column x=35 by 1
+rotate column x=29 by 1
+rotate column x=19 by 1
+rotate column x=7 by 2
+rotate row y=4 by 22
+rotate row y=3 by 5
+rotate row y=1 by 21
+rotate row y=0 by 10
+rotate column x=2 by 2
+rotate column x=0 by 2
+rect 4x2
+rotate column x=46 by 2
+rotate column x=44 by 2
+rotate column x=42 by 1
+rotate column x=41 by 1
+rotate column x=40 by 2
+rotate column x=38 by 2
+rotate column x=37 by 3
+rotate column x=35 by 1
+rotate column x=33 by 2
+rotate column x=32 by 1
+rotate column x=31 by 2
+rotate column x=30 by 1
+rotate column x=28 by 1
+rotate column x=27 by 3
+rotate column x=26 by 1
+rotate column x=23 by 2
+rotate column x=22 by 1
+rotate column x=21 by 1
+rotate column x=20 by 1
+rotate column x=19 by 1
+rotate column x=18 by 2
+rotate column x=16 by 2
+rotate column x=15 by 1
+rotate column x=13 by 1
+rotate column x=12 by 1
+rotate column x=11 by 1
+rotate column x=10 by 1
+rotate column x=7 by 1
+rotate column x=6 by 1
+rotate column x=5 by 1
+rotate column x=3 by 2
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 49x1
+rotate row y=2 by 34
+rotate column x=44 by 1
+rotate column x=40 by 2
+rotate column x=39 by 1
+rotate column x=35 by 4
+rotate column x=34 by 1
+rotate column x=30 by 4
+rotate column x=29 by 1
+rotate column x=24 by 1
+rotate column x=15 by 4
+rotate column x=14 by 1
+rotate column x=13 by 3
+rotate column x=10 by 4
+rotate column x=9 by 1
+rotate column x=5 by 4
+rotate column x=4 by 3
+rotate row y=5 by 20
+rotate row y=4 by 20
+rotate row y=3 by 48
+rotate row y=2 by 20
+rotate row y=1 by 41
+rotate column x=47 by 5
+rotate column x=46 by 5
+rotate column x=45 by 4
+rotate column x=43 by 5
+rotate column x=41 by 5
+rotate column x=33 by 1
+rotate column x=32 by 3
+rotate column x=23 by 5
+rotate column x=22 by 1
+rotate column x=21 by 2
+rotate column x=18 by 2
+rotate column x=17 by 3
+rotate column x=16 by 2
+rotate column x=13 by 5
+rotate column x=12 by 5
+rotate column x=11 by 5
+rotate column x=3 by 5
+rotate column x=2 by 5
+rotate column x=1 by 5`;
+var split = input.split("\n");
+
+function isRotationCommand (element) {
+  if (element[1] === "o") {
+    return true;
+  } else if (element[2] === "o") {
+    return false;
+  }
+}
+
+function initializeBoard (x, y) {
+  var arr = [];
+  for (var i = 0; i < y; i++) {
+    var row = [];
+    for (var j = 0; j < x; j++) {
+      row.push(".");
+    }
+  arr.push(row);
+  }
+  return arr;
+}
+
+
+function parseRect (command) {
+  var regex = new RegExp (/(\d*)x(\d*)/, "g");
+  var match = regex.exec(command);
+  var x = parseInt(match[1]);
+  var y = parseInt(match[2]);
+  return {
+    "x": x,
+    "y": y
+  };
+}
+
+function parseInstructions (string) {
+  var regex = new RegExp(/rotate ([a-z]*) (?:x=|y=)(\d*) by (\d*)/, "g");
+  var match = regex.exec(string);
+  var direction = match[1];
+  var index = parseInt(match[2]);
+  var steps = parseInt(match[3]);
+  return {
+    "direction": direction,
+    "index": index,
+    "steps": steps
+  };
+}
+
+
+function turnOnLights (x, y, array) {
+  for (var i = 0; i < y; i++) {
+    for (var j = 0; j < x; j++) {
+      array[i][j] = 1;
+    }
+  }
+}
+
+
+function rotate (direction, index, number) {
+  if (direction === "row") {
+    for (var i = 0; i < number; i++) {
+      arr[index].unshift(arr[index][arr[index].length-1]);
+      arr[index].splice(arr[index].length-1, 1);
+    }
+  } else if (direction === "column") {
+      for (var i = 0; i < number; i++) {
+        var temp = arr[arr.length-1][index];
+        for (var j = 0; j < arr.length-1; j++) {
+          arr[arr.length-1-j][index] = arr[arr.length-1-j-1][index];
+        }
+        arr[0][index] = temp;
+      }
+  }
+  return arr;
+}
+
+function countLights (array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array[i].length; j++) {
+      if (array[i][j] > 0) {
+        sum++;
+      }
+    }
+  }
+  return sum;
+}
+
+/*
+function makeSubArrays (array) {
+  for (var i = 0; i < array.length; i + 5) {
+    var subArray = [];
+
+  }
+}
+*/
+
+
+// *** MAIN PROGRAM ***
+
+var arr = initializeBoard(50, 6);
+
+for (var i = 0; i < split.length; i++) {
+  if (isRotationCommand(split[i])) {
+    var instr = parseInstructions(split[i]);
+    rotate(instr.direction, instr.index, instr.steps);
+  } else {
+    var rect = parseRect(split[i]);
+    turnOnLights(rect.x, rect.y, arr);
+  }
+}
+
+arr.map(function(row) {
+  console.log(row.join(""))
+});
